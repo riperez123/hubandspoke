e032742f6153a2d5c4711ad46d1dfc8a477fd86e
riperez123 <31898866+riperez123@users.noreply.github.com>
2023-09-12 14:58:53 -0600

    Update main.tf
    Cleaning up code to remove resources that were no longer needed

--- hubNspoke/main.tf
+++ hubNspoke/main.tf
@@ -6,17 +6,3 @@ resource "azurerm_resource_group" "primary-rg" {
   location = var.primary-location
 }
 
-/*resource "azurerm_resource_group" "hubandspoke-secondary-rg" {
-  name     = var.secondary-rg
-  location = var.secondary-location
-}
-
-resource "azurerm_resource_group" "hubandspoke-onprem1-rg" {
-  name     = var.onprem-site1-rg
-  location = var.primary-location
-}
-
-resource "azurerm_resource_group" "hubandspoke-onprem2-rg" {
-  name     = var.onprem-site2-rg
-  location = var.secondary-location
-}*/
